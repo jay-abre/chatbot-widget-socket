cfd7b44a0b95f668c78857d079c325be62341cdb
jcfrongoso <i_justinclyde.frongoso@stratpoint.com>
2024-11-22 14:55:48 +0800

    feat: add rooms

--- controllers/socketController.js
+++ controllers/socketController.js
@@ -1,11 +1,14 @@
 module.exports = (socket) => {
     console.log('A user connected');
 
+    socket.join("some room");
+
     // Listen for messages from the client
     socket.on('message', (data) => {
         console.log('Message received:', data);
+
         // Send a response back to the same client
-        socket.emit('response', `Server says: ${data}`);
+        socket.to("some room").emit('response', `${data}`);
     });
 
     socket.on('disconnect', () => {
